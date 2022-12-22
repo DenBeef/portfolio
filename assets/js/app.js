98d613bdfd29d235f58010f0c5894c865a24d734
DenBeef <91434725+DenBeef@users.noreply.github.com>
2022-12-22 23:38:14 +0000

    just needs projects and customizing

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,5 @@
 
+// Add animation to skills icons
 function mouseOver(elem) {
     var e = elem.id;
     if (e == "css-icon" || e == "html-icon" || e == "js-icon" || e == "ps-icon" || e == "py-icon" || e == "git-icon") {
@@ -10,5 +11,35 @@ function mouseOver(elem) {
   }
 
 
+// Scroll to the top of the page
+const scrollUp = document.querySelector("#scroll-up");
 
+scrollUp.addEventListener("click", ()=> {
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth",
+    });
+});
+
+// Nav hamburgerburger selections
+
+const burger = document.querySelector("#burger-menu");
+const ul = document.querySelector("nav ul");
+const nav = document.querySelector("nav");
+
+burger.addEventListener("click", () => {
+    ul.classList.toggle("show");
+  });
+
+// Close hamburger menu when a link is clicked
+
+// Select nav links
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach((link) =>
+  link.addEventListener("click", () => {
+    ul.classList.remove("show");
+  })
+);
 
